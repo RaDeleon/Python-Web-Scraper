1a46fd3ae1f697d7c59f5c98ee8eb78d0f0f411d
Radeleon <rudy209@comcast.net>
2019-07-25 13:53:05 -0700

    First commit added feature branch

---
+++ scraper.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+requests.get('https://www.youtube.com/results?search_query=python')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO:
+# * Count total number of views for all videos on 1st page of youtube for given keyword
+# * Keyword = Python
